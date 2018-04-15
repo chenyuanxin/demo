f8b8ccec577837a7f39bd9209f66b244e76aaaa6
chenyuanxin <414192768@qq.com>
2018-04-15 17:44:03 +0800

    首次创建工程

---
+++ MySafari/MySafari/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MySafari
+//
+//  Created by Apple on 2018/4/15.
+//  Copyright © 2018年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
