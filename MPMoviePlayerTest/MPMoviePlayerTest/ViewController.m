259831ffbf03c4e8b0baca47c2344299e1c0697d
chenyuanxin <414192768@qq.com>
2018-05-14 08:45:50 +0800

    音频播放测试，展示中间截图

--- MPMoviePlayerTest/MPMoviePlayerTest/ViewController.m
+++ MPMoviePlayerTest/MPMoviePlayerTest/ViewController.m
@@ -22,13 +22,27 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     //播放视频
+//    NSString * path = [[NSBundle mainBundle]pathForResource:@"iphone" ofType:@"mp4"];
+//    NSLog(@"path = %@", path);
+//    NSURL * url = [NSURL fileURLWithPath:path];
+//    _movieController = [[MPMoviePlayerController alloc]initWithContentURL:url];
+//    _movieController.view.frame = CGRectMake(0, 0, 320, 300);
+//    [self.view addSubview:_movieController.view];
+//    [_movieController play];
+    
+    //查看某一时刻的视频截图
     NSString * path = [[NSBundle mainBundle]pathForResource:@"iphone" ofType:@"mp4"];
-    NSLog(@"path = %@", path);
     NSURL * url = [NSURL fileURLWithPath:path];
     _movieController = [[MPMoviePlayerController alloc]initWithContentURL:url];
+    _movieController.controlStyle = MPMovieControlStyleFullscreen;
     _movieController.view.frame = CGRectMake(0, 0, 320, 300);
     [self.view addSubview:_movieController.view];
+    UIImage * image = [_movieController thumbnailImageAtTime:100 timeOption:MPMovieTimeOptionNearestKeyFrame];
+    UIImageView * imageView = [[UIImageView alloc]initWithFrame:CGRectMake(0, 350, 320, 200)];
+    imageView.image = image;
+    [self.view addSubview:imageView];
     [_movieController play];
+    
 }
 
 
