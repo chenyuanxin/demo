492db9e95507fbbef536de86d596154b1bfc867a
chenyuanxin <414192768@qq.com>
2018-05-15 08:15:46 +0800

    视频播放测试

--- MPMoviePlayerViewControllerTest/MPMoviePlayerViewControllerTest/ViewController.m
+++ MPMoviePlayerViewControllerTest/MPMoviePlayerViewControllerTest/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <MediaPlayer/MediaPlayer.h>
 
 @interface ViewController ()
 
@@ -17,6 +18,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+//播放视频
+- (IBAction)playMovie:(UIButton *)sender {
+    NSString * path = [[NSBundle mainBundle]pathForResource:@"iphone" ofType:@"mp4"];
+    NSURL * url = [NSURL fileURLWithPath:path];
+    MPMoviePlayerViewController * controller = [[MPMoviePlayerViewController alloc]initWithContentURL:url];
+    [self presentMoviePlayerViewControllerAnimated:controller];
 }
 
 
